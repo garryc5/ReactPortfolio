a34a05099b8ce563847510a6345239224058c912
garryc5 <gcarver994@iwcc.edu>
2019-10-22 19:12:31 -0500

    bix fix test

--- src/components/SkillsSpotLight/SkillSpotLight.jsx
+++ src/components/SkillsSpotLight/SkillSpotLight.jsx
@@ -27,8 +27,8 @@ class SkillsSpotLight extends React.Component {
     document.getElementById("spotlight").style.borderRadius = `${this.state.w / 3 + 20}% ${this.state.b / 3 + 20}% ${this.state.b / 3 + 20}% ${this.state.w / 3 + 20}%`;
     }
     
-    handleMouseOver = async e => {
-      await clearInterval(this.state.timer);
+    handleMouseOver = e => {
+      clearInterval(this.state.timer);
       document.getElementById("spotlight").style.left = `${e.clientX}px`;
     }
 
@@ -36,9 +36,9 @@ class SkillsSpotLight extends React.Component {
     {
       this.setState({timer: setInterval(this.topDomManipulation, 25)      })
     }
-    handleTouchStart = async e => {
-      await clearInterval(this.state.timer);
-      await document.getElementById("spotlight").style.left = `${e.touches[0].clientX}px`;
+    handleTouchStart = e => {
+      clearInterval(this.state.timer);
+      document.getElementById("spotlight").style.left = `${e.touches[0].clientX}px`;
     }
 
 
